12c897fcfeb89d40fd92606b875c3609dc9a2c13
Afterimage Designs <hello@afterimagedesigns.com>
2019-08-07 14:24:55 +0800

    Add files via upload

--- includes/plugin-list.php
+++ includes/plugin-list.php
@@ -36,20 +36,9 @@ function divi_child_register_required_plugins() {
 
 		// This is an example of how to include a plugin bundled with a theme.
 		array(
-			'name'               => 'Elegant Theme Updater', // The plugin name.
-			'slug'               => 'elegant-themes-updater', // The plugin slug (typically the folder name).
-			'source'             => get_stylesheet_directory() . '/includes/plugins/elegant-themes-updater.zip', // The plugin source.
-			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-			'version'            => '1.2', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
-			'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-			'force_deactivation' => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
-		),
-		array(
 			'name'               => 'Backup Buddy', // The plugin name.
 			'slug'               => 'backupbuddy', // The plugin slug (typically the folder name).
-			'source'             => get_stylesheet_directory() . '/includes/plugins/backupbuddy-6.0.1.0.zip', // The plugin source.
+			'source'             => get_stylesheet_directory() . '/includes/plugins/backup-buddy.zip', // The plugin source.
 			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
 			'version'            => '6.0.1.0', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
 			'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
